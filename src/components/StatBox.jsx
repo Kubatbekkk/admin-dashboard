257415cf1b27cef978d7ae9ebef891ef6aba6136
Kubatbekkk <kubineccastro@gmail.com>
2023-04-01 14:26:15 +0600

    feat: add Dashboard

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -1,5 +1,5 @@
 import { Box, Typography, useTheme } from '@mui/material';
-import { tokens } from '../../theme';
+import { tokens } from '../theme';
 import ProgressCircle from './ProgressCircle';
 
 const StatBox = ({ title, subtitle, icon, progress, increase }) => {
@@ -7,7 +7,7 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box width="100%" m="0 30px">
+    <Box width="100%" m="0 30px" py="5px">
       <Box display="flex" justifyContent="space-between">
         <Box>
           {icon}
@@ -22,24 +22,18 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
         <Box>
           <ProgressCircle progress={progress} />
         </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography
-            variant="h4"
-            fontWeight="bold"
-            sx={{ color: colors.greenAccent[500] }}
-          >
-            {subtitle}
-          </Typography>
-
-          <Typography
-            variant="h5"
-            fontWeight="bold"
-            fontStyle="italic"
-            sx={{ color: colors.greenAccent[600] }}
-          >
-            {increase}
-          </Typography>
-        </Box>
+      </Box>
+      <Box display="flex" justifyContent="space-between" mt="2px">
+        <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+          {subtitle}
+        </Typography>
+        <Typography
+          variant="h5"
+          fontStyle="italic"
+          sx={{ color: colors.greenAccent[600] }}
+        >
+          {increase}
+        </Typography>
       </Box>
     </Box>
   );
